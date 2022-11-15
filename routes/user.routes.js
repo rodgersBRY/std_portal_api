fae933c0ef87c33102ec17c9dd3c478ab385f21f
RodgersX <brianmawira2@gmail.com>
2022-11-15 14:57:36 +0300

    changes II

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const authGuard = require("../authguard");
 
-router.get("/", userController.getModules);
+router.get("/modules", userController.getModules);
 
 module.exports = router;
